13b973b216f5d653af50b354ac0dc32c120a963f
Kris Anderson <CalmCatStudio@gmail.com>
2022-12-24 21:01:16 -0500

    Prepare buttons for functionality

--- calculator.js
+++ calculator.js
@@ -1,1 +1,49 @@
-console.log("test");
+const buttons = document.querySelectorAll('button');
+for (let i = 0; i < buttons.length; i++)
+{
+    let button = buttons[i];
+    let buttonVal = button.textContent;
+    button.addEventListener('click',
+    function() {
+        handleButtonPress(buttonVal);
+    });
+}
+
+function handleButtonPress(buttonVal)
+{
+    if (buttonVal === 'back')
+    {
+        // TODO: Implement backspace
+        console.log('BACKSPACE');
+    }
+    else if (buttonVal === 'clear')
+    {
+        // TODO: Implement clear
+        console.log('CLEAR');
+    }
+    else if (buttonVal === '=')
+    {
+        // TODO: Implement equals
+        console.log('Equals');
+    }
+    else if (isOperator(buttonVal))
+    {
+        // TODO: Implement Operator
+        console.log('Operator'); 
+    }
+    else
+    {
+        // TODO: Enter number into equation.
+        console.log('number');
+    }
+}
+
+function isOperator(buttonVal)
+{
+    if (buttonVal === '+' || buttonVal === '-' || buttonVal === '*' || buttonVal === '/')
+    {
+        return true;
+    }
+    return false;
+}
+
